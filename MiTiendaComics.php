9dd8644ec5bf158fac10f9971f56799dd1081a97
MarvinEsteban <tommyesgon0202@gmail.com>
2025-01-09 11:33:15 +0100

    Deleted notes on MiTienda

--- MiTiendaComics.php
+++ MiTiendaComics.php
@@ -65,12 +65,12 @@ function aplicarDescuentoManga()
 
     foreach ($inventario['accion'] as &$comic) {
         if ($comic['idioma'] == 'Japonés') {
-            $comic['precio'] = $comic['precio'] * 0.7; // Aplicar descuento del 30%
+            $comic['precio'] = $comic['precio'] * 0.7; 
         }
     }
     foreach ($inventario['suspense_terror'] as &$comic) {
         if ($comic['idioma'] == 'Japonés') {
-            $comic['precio'] = $comic['precio'] * 0.7; // Aplicar descuento del 30%
+            $comic['precio'] = $comic['precio'] * 0.7; 
         }
     }
 }
